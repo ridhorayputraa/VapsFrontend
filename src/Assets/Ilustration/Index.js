d731df778341f914ddea725791fcf55b92ab9a58
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-04-26 23:22:27 +0700

    Finishing SuccesSignUpPages

--- src/Assets/Ilustration/Index.js
+++ src/Assets/Ilustration/Index.js
@@ -1,4 +1,5 @@
 import Logo from "./Logo.svg";
 import IlSuccesSignUp from "./ILSuccessSignUp.svg";
+import IlustrationSUcces from "./ILustrationSuccess.svg";
 
-export { Logo, IlSuccesSignUp };
+export { Logo, IlSuccesSignUp, IlustrationSUcces };
--- src/Pages/SuccessSignUp/SuccessSignUp.tsx
+++ src/Pages/SuccessSignUp/SuccessSignUp.tsx
@@ -1,14 +1,45 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { IlustrationSUcces } from "../../Assets";
+import { Button, Gap } from "../../Components";
 
 const SuccessSignUp = () => {
   return (
-    <View>
-      <Text>SuccessSignUp</Text>
+    <View style={styles.page}>
+      <IlustrationSUcces />
+      <Gap height={31} />
+      <Text style={styles.title}>Yeay! Completed</Text>
+      <Gap height={6} />
+      <Text style={styles.SubTitle}>Now you are able to order</Text>
+      <Text style={styles.SubTitle}>some products as a self-reward</Text>
+      <View style={styles.buttonContainer}>
+        <Gap height={30} />
+        <Button text="Find Vapss" />
+      </View>
     </View>
   );
 };
 
 export default SuccessSignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 20,
+    color: "#020202",
+  },
+  SubTitle: {
+    fontFamily: "Poppins-Light",
+    fontSize: 14,
+    color: "#8d92a3",
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: 80,
+  },
+});
